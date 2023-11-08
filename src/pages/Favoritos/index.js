09ea0927261f56d33b38f7ed8e024c7a8ea08df6
PeuStone <pedrohmotaaa@gmail.com>
2023-11-08 10:16:24 -0300

    Finalizando implementação da funçao de favoritar

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,12 @@
 import Banner from 'components/Banner';
 import styles from './Favoritos.module.css';
 import Titulo from 'components/Titulo';
+import { useFavoritoContext } from 'contextos/Favoritos';
+import Card from 'components/Card';
 
 function Favoritos() {
+    const { favorito } = useFavoritoContext();
+
     return (
         <>
             <Banner imagem="favoritos" />
@@ -10,7 +14,9 @@ function Favoritos() {
                 <h1>Meus Favoritos</h1>
             </Titulo>
             <section className={styles.container}>
-
+                {favorito.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
             </section>
         </>
     )
